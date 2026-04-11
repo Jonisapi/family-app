9fc4058fcd4ea89fbf5e4e99c4583f4983b26280
Jonathan Sapir <jonathan@woodandbeyond.com>
2026-04-11 11:10:45 +0100

    Clean up leftover fix scripts

--- fix-import2.cjs
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let family = fs.readFileSync('src/pages/Family.tsx', 'utf8');
-family = family.replace(
-  "import { useFamily, useTodaySugarByMember } from '../contexts/FamilyContext'",
-  "import { useFamily, useTodaySugarByMember } from '../contexts/FamilyContext'\nimport { updateFamily } from '../lib/familyService'"
-);
-fs.writeFileSync('src/pages/Family.tsx', family, 'utf8');
-console.log('Done! Has updateFamily:', family.includes('updateFamily'));
